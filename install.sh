e486f41282f7768c159a238093692a27dad10a83
paralleltree <paralleltree@outlook.com>
2015-06-13 16:50:28 +0900

    [vim] Add NeoBundle settings

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@
 # vim
 mkdir -p ~/.vim/colors
 wget -P ~/.vim/colors https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
